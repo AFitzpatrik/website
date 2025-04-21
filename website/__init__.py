6830e1749fd0f357ac58d4aa5bea06df8dfdbd7c
franců <francupatrik@seznam.cz>
2025-04-21 17:06:44 +0200

    14:00 Spuštění web serveru

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,9 @@
 #když mám ve složce __init__.py, tak se složka bere jako modul(package/balíček)
+from flask import Flask
+
+#Zdefinování funkce create_app, která vytvoří a vrátí instanci Flask aplikace. Založení Flask aplikace
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'mysecretkey'
+
+    return app
